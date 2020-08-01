f676c5c9cbfaa3dd30432eeb820af5d7be5894c9
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-07-31 19:33:08 -0700

    trying something new

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,33 +2,38 @@ import React from "react";
 
 import Layout from "../components/layout";
 
-const verticalLeft = {
+const rotate = {
+  transform: "rotate(-180deg)",
+};
+
+const vertical = {
   color: "white",
   background: "black",
   padding: 40,
   textTransform: "uppercase",
-  transform: "rotate(-180deg)",
   writingMode: "vertical-rl",
-  letterSpacing: ".3em",
   textAlign: "center",
+  letterSpacing: ".3em",
   width: 100,
   fontSize: 44,
   fontFamily: "Helvetica Neue",
   fontWeight: 500,
 };
 
-const horizontalRight = {
-  color: "white",
-  background: "black",
-  padding: 40,
-  textTransform: "uppercase",
-  writingMode: "vertical-rl",
-  textAlign: "center",
-  letterSpacing: ".3em",
-  width: 100,
-  fontSize: 44,
-  fontFamily: "Helvetica Neue",
-  fontWeight: 500,
+const cameraStyle = {
+  margin: 12,
+  backgroundColor: "red",
+  height: "40%",
+  width: 400,
+  borderRadius: 15,
+};
+
+const chatStyle = {
+  margin: 12,
+  backgroundColor: "purple",
+  height: "60%",
+  width: 400,
+  borderRadius: 15,
 };
 
 const layout = {margin: 0, height: 1200, width: 1920, display: "flex"};
@@ -36,9 +41,14 @@ const layout = {margin: 0, height: 1200, width: 1920, display: "flex"};
 const IndexPage = () => (
   <Layout>
     <section style={layout}>
-      <div style={verticalLeft}>bdougie.live</div>
+      <div style={{...vertical, ...rotate}}>bdougie.live</div>
       <p style={{width: "100%"}}></p>
-      <div style={horizontalRight}>opensauced.pizza</div>
+
+      <div style={{height: 1200, display: "flex", flexDirection: "column"}}>
+        <div style={chatStyle}>chatbox</div>
+        <div style={cameraStyle}>camera</div>
+      </div>
+      <div style={vertical}>opensauced.pizza</div>
     </section>
   </Layout>
 );
